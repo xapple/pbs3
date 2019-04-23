1324f9dce7d70b71d11f3fca41ffb8a09fb207ff
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-04-23 16:23:40 +0200

    Replace setup.py with a simpler version

--- setup.py
+++ setup.py
@@ -1,56 +1,13 @@
-import os
-import sys
-import pbs
-import subprocess
-
-try: from distutils.core import setup
-except ImportError: from setuptools import setup
-
-
-if sys.argv[1] == "test":
-    def run_test(version):
-        py_version = "python%s" % version
-        py_bin = pbs.which(py_version)
-
-        if py_bin:
-            print("Testing %s" % py_version.capitalize())
-
-            p = subprocess.Popen([py_bin, "test.py"] + sys.argv[2:])
-            p.wait()
-        else:
-            print("Couldn't find %s, skipping" % py_version.capitalize())
-
-    versions = ("2.6", "2.7", "3", "3.1", "3.2")
-
-    for version in versions:
-        run_test(version)
-
-    exit(0)
-
+from setuptools import setup
 
 setup(
-    name="pbs",
-    version=pbs.__version__,
-    description="Python subprocess wrapper",
-    author="Andrew Moffat",
-    author_email="andrew.robert.moffat@gmail.com",
-    url="https://github.com/amoffat/pbs",
-    license="MIT",
-    py_modules=["pbs"],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.1",
-        "Programming Language :: Python :: 3.2",
-        "Topic :: Software Development :: Build Tools",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-)
+        name             = 'pbs3',
+        version          = '3.0.0',
+        description      = 'pbs3 is a python package for launching external processes easily.',
+        long_description = open('README.md').read(),
+        license          = 'MIT',
+        url              = 'http://github.com/xapple/pbs3/',
+        author           = 'Lucas Sinclair',
+        author_email     = 'lucas.sinclair@me.com',
+        py_modules       = ["pbs3"],
+    )
